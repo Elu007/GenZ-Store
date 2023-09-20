3f40358054107e4782f4ed97834e89db56f6b650
Elaf <skelaf2000@gmail.com>
2023-09-20 21:28:57 +0530

    Some Navbar updates

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { useStateValue } from '../StateProvider';
@@ -10,6 +10,15 @@ const NavbarContainer = styled.nav`
   align-items: center;
   background-color: #333;
   padding: 1rem;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  z-index: 100;
+
+  &.sticky {
+    background-color: #333; /* Sticky background color */
+    box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.2); /* Add a shadow for visual effect */
+  }
 
   @media (max-width: 768px) {
     flex-direction: column;
@@ -32,6 +41,10 @@ const Button = styled.button`
   cursor: pointer;
   margin-left: 1rem;
   margin-right: 1rem;
+  img{
+    height: 16px;
+    width: 23px;
+  }
 `;
 const Utils = styled.div`
   display: flex;
@@ -39,14 +52,36 @@ const Utils = styled.div`
 `;
 const Navbar = () => {
   const [{ basket }, dispatch] = useStateValue();
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    // Function to handle scroll event
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+      // eslint-disable-next-line
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
 
   return (
-    <NavbarContainer>
+    <NavbarContainer className={isSticky ? 'navbar sticky' : 'navbar'} isSticky={isSticky}>
       <Link to={"/"}><Logo src="/images/storelogo.svg" alt="logo"/></Link>
       {/* Here I will add the user name and greetings */}
       <Utils>
         <Link to={"/login"}><Button>Login</Button></Link>
-        <Link to={"/cart"}><Button>Cart {basket.length}</Button></Link>
+        <Link to={"/cart"}><Button><img src="/images/cart.png" alt="cart" /> {basket.length}</Button></Link>
       </Utils>
     </NavbarContainer>
   );
