b68cab4521e536cc6213ccb1f5c9274d12763157
Elaf <skelaf2000@gmail.com>
2023-10-14 11:06:54 +0530

    Added access controls

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -15,6 +15,16 @@ mongoose.connect(DB).then(() =>{
 
 const User = require("../models/User");
 
+// Add this middleware to set the appropriate CORS headers
+// This handles the cors error while deploying
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://gen-z-store.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 const allowedOrigins = ['https://gen-z-store.vercel.app'];
 
 router.use(
